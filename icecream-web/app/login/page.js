7923674105055c910d8b4c5d8ef77cf41c73eedb
NucyLoodle <lucymeredith3@gmail.com>
2025-02-10 15:04:58 +0000

    change id tags

--- icecream-web/app/login/page.js
+++ icecream-web/app/login/page.js
@@ -3,27 +3,27 @@ import Link from 'next/link';
 
 function LoginPage() {
     return(
-                <div id='login-page-component'>
-                    <div id='login-content-container'>
-                        <div id='login-register-button-container'>
-                            <button id='login-button'>Login</button>
-                            <button id='register-button'>
+                <div id='lg-login-page-component'>
+                    <div id='lg-login-content-container'>
+                        <div id='lg-login-register-button-container'>
+                            <button id='lg-login-button'>Login</button>
+                            <button id='lg-register-button'>
                                 <Link href='/registration'>
                                     Register
                                 </Link>
                             </button>
                         </div>
-                        <div id='login-form-container'>
-                            <form id='login-form'>
+                        <div id='lg-login-form-container'>
+                            <form id='lg-login-form'>
                                 <label htmlFor='email'>Email:</label>
-                                <input type='text' id='email' name='email' placeholder='Email' required></input>
+                                <input type='text' id='lg-email' name='email' placeholder='Email' required></input>
                                 <label htmlFor='password'>Password:</label>
-                                <input type='password' id='password' name='password' placeholder='Password' required></input>
+                                <input type='password' id='lg-password' name='password' placeholder='Password' required></input>
                                 <div id="remember-me-container">  
-                                    <input type="checkbox" id="remember-me" name="remember-me"></input>
+                                    <input type="checkbox" id="lg-remember-me" name="remember-me"></input>
                                     <label htmlFor="remember-me">Remember me?</label>
                                 </div>
-                                <button id='login-button'>Login</button>
+                                <button id='lg-submit-login-button'>Login</button>
                             </form>
                         </div>
                     </div>
