0e3501f2fd9b1727bd0a309bf04ea2b0a0586c28
NucyLoodle <lucymeredith3@gmail.com>
2025-02-10 10:34:12 +0000

    link to login page

--- icecream-web/app/Header/Header.js
+++ icecream-web/app/Header/Header.js
@@ -1,5 +1,6 @@
 import './Header.css'
 import Image from 'next/image'
+import Link from 'next/link';
 import logo from '../../public/icecream_logo.png';
 
 function Header() {
@@ -9,7 +10,13 @@ function Header() {
                 <Image src={logo} alt="strawberry and vanilla ice cream cone" />
                 <h1>Ice Cream Tracker</h1>
             </div>
-            <button id="get-started-btn">Get Started</button>
+            <button id="get-started-btn">
+                <Link
+                    href="/login"
+                    id="lp-get-started">
+                    Let's get started!
+                </Link>
+            </button>
         </div>
 
     );
