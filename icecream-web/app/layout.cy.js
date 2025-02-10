b13fec143dad2dd20dfce6a4c1868292fa8a80c5
NucyLoodle <lucymeredith3@gmail.com>
2025-02-10 14:40:11 +0000

    cypress component test for layout

---
+++ icecream-web/app/layout.cy.js
@@ -0,0 +1,17 @@
+import Layout from './layout';
+
+describe('<Layout />', () => {
+  it('renders Header, Footer, and children correctly', () => {
+    cy.mount(
+        <Layout>
+          <div data-testid="child-content">Test Content</div>
+        </Layout>
+      );
+
+    cy.get('#header-component').should('exist').and('contain', 'Ice Cream Tracker');
+    cy.get('#footer-component').should('exist').and('contain', 'Meredith Designs');
+    cy.get('[data-testid="child-content"]').should('exist').and('contain', 'Test Content');
+    cy.get('.content-container').should('exist');
+    cy.get('.children-container').should('exist');
+  });
+});
