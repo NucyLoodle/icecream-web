ef9163abde54465b14fa1843c74b30267f4c0c3f
NucyLoodle <lucymeredith3@gmail.com>
2025-02-11 20:33:42 +0000

    initialise credentials provider

--- icecream-web/auth.js
+++ icecream-web/auth.js
@@ -1,5 +1,27 @@
 import NextAuth from "next-auth"
- 
+import Credentials from "next-auth/providers/credentials"
+
+// import some salted hash function
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [],
-})
+  providers: [
+    Credentials({
+        credentials: {
+            email: {},
+            password: {},    
+        },
+        authorize: async (credentials) => {
+            let user = null
+            // salt and hash the password
+            // check if user exists via email
+
+            if (!user) {
+                //no user found
+                // register user?
+                throw new Error("Invalid credentials.")
+            }
+            // return user object
+            return user
+        },
+    }),
+  ],
+});
