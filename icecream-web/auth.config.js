c547ab7441081e7cf02fda33ba0da4224478802b
NucyLoodle <lucymeredith3@gmail.com>
2025-02-12 19:51:32 +0000

    fix auth.config file

--- icecream-web/auth.config.js
+++ icecream-web/auth.config.js
@@ -1,6 +1,20 @@
-/** @type {import("next-auth").NextAuthConfig} */
 export const authConfig = {
-    pages: {
-      signIn: "/login", //specify the route for custom login page
-    },
-  };
+  pages: {
+    signIn: '/login',
+  },
+  callbacks: {
+    // authorized({ auth, request: { nextUrl } }) {
+    //   const isLoggedIn = !!auth?.user;
+    //   const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
+
+    //   if (isOnDashboard) {
+    //     if (isLoggedIn) return true;
+    //     return false; // Redirect unauthenticated users to login page
+    //   } else if (isLoggedIn) {
+    //     return Response.redirect(new URL('/dashboard', nextUrl));
+    //   }
+    //   return true;
+    // },
+  },
+  providers: [], // Add providers with an empty array for now
+};
