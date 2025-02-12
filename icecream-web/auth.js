be9959c9ae4208ad40f12999c29ab328149855c2
NucyLoodle <lucymeredith3@gmail.com>
2025-02-12 13:18:48 +0000

    spread authConfig

--- icecream-web/auth.js
+++ icecream-web/auth.js
@@ -1,8 +1,11 @@
 import NextAuth from "next-auth"
+import { authConfig } from "./auth.config";
 import Credentials from "next-auth/providers/credentials"
+import bcrypt from "bcryptjs";
 
-// import some salted hash function
-export const { handlers, signIn, signOut, auth } = NextAuth({
+
+export const { signIn, signOut, auth } = NextAuth({
+    ...authConfig, //NextAuth receives all key-value pairs from authConfig as if they were written directly inside the object.
   providers: [
     Credentials({
         credentials: {
@@ -12,14 +15,19 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
         authorize: async (credentials) => {
             let user = null
             // salt and hash the password
+            const hashedPassword = await bcrypt.hash(password, 10)
             // check if user exists via email
-
+            user = await sql`
+                SELECT * FROM users WHERE email = ${credentials.email}
+            `
             if (!user) {
                 //no user found
                 // register user?
+                console.log("No user found.")
                 throw new Error("Invalid credentials.")
             }
             // return user object
+            console.log(user)
             return user
         },
     }),
