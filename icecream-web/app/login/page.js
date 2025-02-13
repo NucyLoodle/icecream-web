735f88b4734e90b40cca4389678338a365d86db7
NucyLoodle <lucymeredith3@gmail.com>
2025-02-13 06:20:22 +0000

    wrap form in suspense

--- icecream-web/app/login/page.js
+++ icecream-web/app/login/page.js
@@ -5,7 +5,7 @@ import Link from 'next/link';
 import { useForm } from 'react-hook-form';
 import { ErrorMessage } from "@hookform/error-message";
 
-import { useActionState } from 'react';
+import { Suspense, useActionState } from 'react';
 import { authenticate } from '@/app/lib/actions';
 import { useSearchParams } from 'next/navigation';
 
@@ -30,48 +30,50 @@ function LoginPage() {
                             </button>
                         </div>
                         <div id='lg-login-form-container'>
-                            <form id='lg-login-form' 
-                            action={formAction}                            
-                            noValidate>
-                                <label htmlFor='lg-email'>Email:</label>
-                                <input
-                                    id='lg-email'
-                                    type='email'
-                                    placeholder="someone@email.com"
-                                    {...register("email", {
-                                        required: "Email is required.",
-                                        pattern: {
-                                            value: /^\S+@\S+$/i,
-                                            message: "Invalid email format."
-                                        }
-                                    })}
-                                    autoComplete="email"
-                                />
-                                <ErrorMessage errors={errors} name="email" render={({ message }) => <p className="error">{message}</p>} />
-                                
-
-
-                                <label htmlFor='lg-password'>Password:</label>
+                            <Suspense>
+                                <form id='lg-login-form' 
+                                action={formAction}                            
+                                noValidate>
+                                    <label htmlFor='lg-email'>Email:</label>
                                     <input
-                                        id='lg-password'
-                                        type='password'
-                                        placeholder='Password'
-                                        {...register("password", {
-                                            required: "Password is required.",
+                                        id='lg-email'
+                                        type='email'
+                                        placeholder="someone@email.com"
+                                        {...register("email", {
+                                            required: "Email is required.",
+                                            pattern: {
+                                                value: /^\S+@\S+$/i,
+                                                message: "Invalid email format."
+                                            }
                                         })}
-                                />
-                                <ErrorMessage errors={errors} name="password" render={({ message }) => <p className="error">{message}</p>} />
-                                
-                                
-                                
-                                <div id="lg-remember-me-container">  
-                                    <input type="checkbox" id="lg-remember-me" name="remember-me"></input>
-                                    <label htmlFor="lg-remember-me">Remember me?</label>
-                                </div>
+                                        autoComplete="email"
+                                    />
+                                    <ErrorMessage errors={errors} name="email" render={({ message }) => <p className="error">{message}</p>} />
+                                    
 
-                                <button id='lg-submit-login-button' type="submit" name="redirectTo" value={callbackUrl}>Login</button>
-                                <div>{errorMessage}</div>
-                            </form>
+
+                                    <label htmlFor='lg-password'>Password:</label>
+                                        <input
+                                            id='lg-password'
+                                            type='password'
+                                            placeholder='Password'
+                                            {...register("password", {
+                                                required: "Password is required.",
+                                            })}
+                                    />
+                                    <ErrorMessage errors={errors} name="password" render={({ message }) => <p className="error">{message}</p>} />
+                                    
+                                    
+                                    
+                                    <div id="lg-remember-me-container">  
+                                        <input type="checkbox" id="lg-remember-me" name="remember-me"></input>
+                                        <label htmlFor="lg-remember-me">Remember me?</label>
+                                    </div>
+
+                                    <button id='lg-submit-login-button' type="submit" name="redirectTo" value={callbackUrl}>Login</button>
+                                    <div>{errorMessage}</div>
+                                </form>
+                            </Suspense>
                         </div>
                     </div>
                 </div>
