103687f4047655a1cc27a7a74406a94d68c3e0cd
NucyLoodle <lucymeredith3@gmail.com>
2025-02-13 06:29:30 +0000

    import login form to fix suspense error

--- icecream-web/app/login/page.js
+++ icecream-web/app/login/page.js
@@ -3,8 +3,8 @@
 import './LoginPage.css';
 import Link from 'next/link';
 import { useForm } from 'react-hook-form';
-import { ErrorMessage } from "@hookform/error-message";
 
+import LoginForm from '@/app/ui/login-form';
 import { Suspense, useActionState } from 'react';
 import { authenticate } from '@/app/lib/actions';
 import { useSearchParams } from 'next/navigation';
@@ -31,48 +31,7 @@ function LoginPage() {
                         </div>
                         <div id='lg-login-form-container'>
                             <Suspense>
-                                <form id='lg-login-form' 
-                                action={formAction}                            
-                                noValidate>
-                                    <label htmlFor='lg-email'>Email:</label>
-                                    <input
-                                        id='lg-email'
-                                        type='email'
-                                        placeholder="someone@email.com"
-                                        {...register("email", {
-                                            required: "Email is required.",
-                                            pattern: {
-                                                value: /^\S+@\S+$/i,
-                                                message: "Invalid email format."
-                                            }
-                                        })}
-                                        autoComplete="email"
-                                    />
-                                    <ErrorMessage errors={errors} name="email" render={({ message }) => <p className="error">{message}</p>} />
-                                    
-
-
-                                    <label htmlFor='lg-password'>Password:</label>
-                                        <input
-                                            id='lg-password'
-                                            type='password'
-                                            placeholder='Password'
-                                            {...register("password", {
-                                                required: "Password is required.",
-                                            })}
-                                    />
-                                    <ErrorMessage errors={errors} name="password" render={({ message }) => <p className="error">{message}</p>} />
-                                    
-                                    
-                                    
-                                    <div id="lg-remember-me-container">  
-                                        <input type="checkbox" id="lg-remember-me" name="remember-me"></input>
-                                        <label htmlFor="lg-remember-me">Remember me?</label>
-                                    </div>
-
-                                    <button id='lg-submit-login-button' type="submit" name="redirectTo" value={callbackUrl}>Login</button>
-                                    <div>{errorMessage}</div>
-                                </form>
+                                <LoginForm />
                             </Suspense>
                         </div>
                     </div>
