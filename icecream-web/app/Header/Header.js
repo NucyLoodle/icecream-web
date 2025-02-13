d2597c81f5e720a69cb908edca60de6eea8cb1f5
NucyLoodle <lucymeredith3@gmail.com>
2025-02-13 20:55:50 +0000

    change header depending on login status

--- icecream-web/app/Header/Header.js
+++ icecream-web/app/Header/Header.js
@@ -2,24 +2,32 @@ import './Header.css'
 import Image from 'next/image'
 import Link from 'next/link';
 import logo from '../../public/icecream_logo.png';
+import { getSession } from "@/app/lib/getSession";
 
-function Header() {
+export default async function Header() {
+    const session = await getSession();
+    console.log(session);
+    const isLoggedIn = !!session;
+
     return(
         <div id="header-component">
             <div id="logo-container">
                 <Image src={logo} alt="strawberry and vanilla ice cream cone" />
                 <h1>Ice Cream Tracker</h1>
             </div>
-            <button id="get-started-btn">
+            {isLoggedIn ? (
+                <div>sign out</div> 
+            ) : (
+                <button id="get-started-btn">
                 <Link
                     href="/login"
                     id="lp-get-started">
                     Let's get started!
                 </Link>
             </button>
+            )}
         </div>
 
     );
 };
 
-export default Header;
