fac4ced506f95d5835eea8a12048a3a73a958554
NucyLoodle <lucymeredith3@gmail.com>
2025-02-13 06:15:42 +0000

    add temporary logout for testing

--- icecream-web/app/dashboard/page.js
+++ icecream-web/app/dashboard/page.js
@@ -1,4 +1,18 @@
+import { signOut } from '@/auth';
+
 
 export default function Dashboard() {
     console.log("logged in")
+    return(
+        <form
+        action={async () => {
+          'use server';
+          await signOut({ redirectTo: '/' });
+        }}
+      >
+        <button>
+          Sign Out
+        </button>
+      </form>
+    )   
 }
