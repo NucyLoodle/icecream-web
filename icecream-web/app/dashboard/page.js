486bbf736cb7f4bc754c7d3a6b479a98de763972
NucyLoodle <lucymeredith3@gmail.com>
2025-02-14 20:06:45 +0000

    build and style dashboard page

--- icecream-web/app/dashboard/page.js
+++ icecream-web/app/dashboard/page.js
@@ -1,9 +1,21 @@
+import './dashboard.css';
+
 
 export default function Dashboard() {
     console.log("logged in")
     return(
-        <div>
-          Dashboard
+        <div id='db-dashboard-component'>
+          <div id='db-content-container'>
+            <h1 id='db-header'>Vans near me</h1>
+            <div id='db-van-info-container'>
+              <div id='db-van-list'>
+
+              </div>
+              <div id='db-map-container'>
+
+              </div>
+            </div>
+          </div>
         </div>
     )   
 }
