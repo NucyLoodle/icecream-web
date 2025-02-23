7afa35f3c06f9570e7e049f242832c40ba2dcda9
NucyLoodle <lucymeredith3@gmail.com>
2025-02-23 14:01:23 +0000

    restore NextAuth

--- icecream-web/auth.js
+++ icecream-web/auth.js
@@ -18,34 +18,30 @@ async function getUser(email) { //queries the db for a user with the given email
     }
   }
 
-export const { signIn, signOut, auth } = NextAuth({
-    ...authConfig, //NextAuth receives all key-value pairs from authConfig as if they were written directly inside the object.
-  providers: [
-    Credentials({
-        credentials: {
-            email: {},
-            password: {},    
-        },
-        authorize: async (credentials) => {
-          if (!credentials || !credentials.email || !credentials.password) {
-              return null; // Ensure credentials exist
+  export const { auth, signIn, signOut } = NextAuth({
+    ...authConfig,
+    providers: [
+      Credentials({
+        async authorize(credentials) {
+          const parsedCredentials = z
+            .object({ email: z.string().email(), password: z.string().min(6) })
+            .safeParse(credentials);
+   
+          if (parsedCredentials.success) {
+            const { email, password } = parsedCredentials.data;
+            const user = await getUser(email);
+            if (!user) return null;
+            const passwordsMatch = await bcrypt.compare(password, user.password);
+ 
+            if (passwordsMatch) {
+				await createSession(user.email)
+				return user;
+			}
           }
-          const email = credentials.email.trim();
-          const password = credentials.password;
-
-          const user = await getUser(email);
-
-          if (!user) return null;
-          const passwordsMatch = await bcrypt.compare(password, user.password);
-  
-          if (passwordsMatch) {
-            await createSession(user.email)
-            return user;
-               
-          }
-          console.log('Invalid credentials');
+		  console.log('Invalid credentials');
           return null;
-      },
-    }),
-  ],
-});
+        },
+      }),
+    ],
+  });
+
