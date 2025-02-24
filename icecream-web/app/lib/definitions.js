5bef3cab571ed3819eff7a238fd2da7aae418439
NucyLoodle <lucymeredith3@gmail.com>
2025-02-24 14:21:48 +0000

    edit password message

--- icecream-web/app/lib/definitions.js
+++ icecream-web/app/lib/definitions.js
@@ -4,7 +4,7 @@ export const signUpFormSchema = z.object({
   email: z.string().email({ message: 'Please enter a valid email.' }).trim(),
   password: z
     .string()
-    .min(8, { message: 'Be at least 8 characters long' })
+    .min(8, { message: 'Password must be at least 8 characters.' })
     .regex(/[a-zA-Z]/, { message: 'Contain at least one letter.' })
     .regex(/[0-9]/, { message: 'Contain at least one number.' })
     .regex(/[^a-zA-Z0-9]/, {
