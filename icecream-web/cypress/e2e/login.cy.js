aa46f0f0e67bf126fce25949574b029c8d54e745
NucyLoodle <lucymeredith3@gmail.com>
2025-02-24 14:22:18 +0000

    add login test

--- icecream-web/cypress/e2e/login.cy.js
+++ icecream-web/cypress/e2e/login.cy.js
@@ -4,7 +4,7 @@ describe('logging in', () => {
 
     cy.get('#get-started-btn').click();
     cy.location('pathname').should('include', 'login')
-
+    cy.get('#lg-login-button').should('have.css', 'background-color', 'rgb(238, 224, 96)');
     cy.get('#lg-email').type(Cypress.env('TEST_EMAIL'));
     cy.get('#lg-password').type(Cypress.env('TEST_PASSWORD'));
     cy.get('#lg-submit-login-button').click();
@@ -16,12 +16,22 @@ describe('logging in', () => {
 
     cy.get('#get-started-btn').click();
     cy.location('pathname').should('include', 'login')
-
-    cy.get('#lg-email').type('email');
+    cy.get('#lg-login-button').should('have.css', 'background-color', 'rgb(238, 224, 96)');
+    cy.get('#lg-email').type('email@email.com');
     cy.get('#lg-password').type('password');
     cy.get('#lg-submit-login-button').click();
     cy.location('pathname').should('include', 'login');
     cy.get('#lg-error-message').should('contain', 'Invalid credentials');
+    });
+
+    it('identifies invalid email addresses', () => {
+      cy.visit('/');
+      cy.get('#get-started-btn').click();
+      cy.location('pathname').should('include', 'login')
+      cy.get('#lg-login-button').should('have.css', 'background-color', 'rgb(238, 224, 96)');
+      cy.get('#lg-email').type('email');
+      cy.get('.lg-email-error').should('contain', 'Invalid email format.');
+
 
     });
 })
